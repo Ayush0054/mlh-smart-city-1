4cde2fdaf43358da6ec9a68ee249e609441e4a3a
Ayush0054 <ayush5aprill@gmail.com>
2023-07-19 21:40:48 +0530

    state update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,23 +9,46 @@ import SingleIssue from "./pages/singleIssue/singleIssue";
 import Navbar from "./components/navbar/navbar";
 import Pending from "./pages/pending/pending";
 import Completed from "./pages/completed/completed";
-
+import { useNavigate } from "react-router-dom";
 import "./app.css";
+import { useEffect, useState } from "react";
+import { localStorageUser } from "./utils/globalConstants";
 
 function App() {
+  const [userData, setUserData] = useState({});
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function fetchUserData() {
+      const data = await JSON.parse(localStorage.getItem(localStorageUser));
+      if(data){
+        
+     
+        setUserData(data);
+      }else{
+        navigate("/login");
+     
+      }
+    }
+    fetchUserData();
+  }, []);
   return (
     <div className='app'>
-      <Navbar />
+      <Routes>
+      <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+ 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+     
         <Route path='/profile' element={<Profile />} />
         <Route path='/issues' element={<Issues />} />
         <Route path='/pending' element={<Pending />} />
         <Route path='/completed' element={<Completed />} />
         <Route path='/issues/:issue' element={<SingleIssue />} />
       </Routes>
+
     </div>
   );
 }
